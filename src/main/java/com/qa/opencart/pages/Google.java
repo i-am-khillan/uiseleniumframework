1146cec4f4623614b78609619382ed9c77effedb
i-am-khillan <khillan.i.verma@gmail.com>
2026-01-05 15:33:13 +0530

    Update Google.java

--- src/main/java/com/qa/opencart/pages/Google.java
+++ src/main/java/com/qa/opencart/pages/Google.java
@@ -9,4 +9,8 @@ public class Google {
 		System.out.println("khillan---login page");
 		System.out.println(test);
 	}
+public void forgetPassword() {
+		System.out.println("sahil---forget page");
+		System.out.println(test);
+	}
 }
